41dc25df6080f0ac73c94d122c8f486ff9814520
Krzysztof <127622188+kr1s6@users.noreply.github.com>
2026-04-07 00:15:23 +0200

    Update: buy-and-sell-crypto - submission-14

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-14.java
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-14.java
@@ -1,12 +1,12 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int max = 0;
+        int res = 0;
         int buy = prices[0];
         for(int sell : prices){
-            max = Math.max(max, sell - buy);
             buy = Math.min(buy, sell);
+            res = Math.max(res, sell - buy);
         }
-        
-        return max;
+
+        return res;
     }
 }
