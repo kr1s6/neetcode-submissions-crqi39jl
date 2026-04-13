ada38dd1f4dc185f451f7ff4d39b99f56f377ff5
Krzysztof <127622188+kr1s6@users.noreply.github.com>
2026-04-13 20:02:45 +0200

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        while(l < r){
+            int mid = l + (r-l) / 2;
+            if(nums[mid] == target) return mid;
+            if(nums[mid] > target){
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        if(nums[l] == target){
+            return l;
+        }
+        return -1;
+    }
+}
